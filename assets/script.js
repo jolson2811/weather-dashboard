8bc841aa2134abc8f94fc3ff167f3c68ade72501
jolson2811 <jolson2811@gmail.com>
2022-03-20 16:52:52 -0600

    added gitignore & moved API key

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 function getWeather(searchInput) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=e61750891a4fdfde0e65aaf0535b7f1f";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=" + config.weather;
     
     fetch(apiUrl).then(function(response){
         console.log("functionfired");
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
+
+
+
               });
         }
     })
